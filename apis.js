c68beeb0e576d7255cf9ab7c4e1fd2e804657bea
wkyuu <tataramoriko@163.com>
2023-02-14 16:23:17 +0800

    重构了识图的代码逻辑

--- apis.js
+++ apis.js
@@ -1,3 +1,5 @@
+import { segment } from 'oicq'
+
 import lodash from 'lodash'
 import moment from 'moment'
 import fetch from "node-fetch"
@@ -18,6 +20,19 @@ const codes = {
     200: '成功请求',
     230: 'key 错误或为空',
     250: '数据返回为空',
+}
+
+const tags = {
+    title: '标题',
+    pixiv_id: 'pixiv id',
+    member_name: 'pixiv 作者名',
+    member_id: 'pixiv 作者 id',
+    danbooru_id: 'danbooru id',
+    gelbooru_id: 'gelbooru id',
+    creator: '作者',
+    material: '主题',
+    characters: '角色',
+    source: '图片来源',
 }
 
 // 舔狗日记
@@ -90,42 +105,137 @@ export class saucenao extends plugin {
             priority: 5000,
             rule: [
                 {
-                    reg: '^#?识图测试$',
+                    reg: '^#?识图$',
                     fnc: 'saucenaoCheckType'
                 }
             ]
         })
     }
 
-    async saucenaoSearch(e) {
+    async saucenaoSearch(e, numres = 3, similarityRate = 70) {
 
         if (apis.saucenao == '') {
-            e.reply('未提供有效的 api_key')
+            this.e.reply('未提供有效的 api_key')
             return
         }
 
-        let imgUrl = e.img[0]
+        let saucenaoUrl = 'https://saucenao.com/search.php'
+        let imgUrl = this.e.img[0]
         let response = await axios.get(
-            'https://saucenao.com/search.php',
+            saucenaoUrl,
             {
                 params: {
                     url: imgUrl,
                     db: 999,
-                    api_key: apis.saucenao,
+                    api_key: apis.saucenao, // 需要到 saucenao.com 获取 api_key
                     output_type: 2,
-                    numres: 3
+                    numres: numres   // 返回多少个结果
                 }
             }
         ).catch((error) => {
             if (error) {
-                e.reply(`识图 api 无反应, 状态：${error}`, true)
+                this.e.reply(`识图 api 无反应, 状态：${error}`, true, { recallMsg: 90 })
                 logger.info(response)
                 return
             }
         })
+
         let responseData = response.data.results
-        logger.info(responseData)
+        let isSelectArr = []
+        let msg
+
+        for (let obj1 of responseData) {
+            if (obj1.header.similarity >= similarityRate) {
+                isSelectArr.push([obj1, obj1.header.similarity])
+            }
+        }
+
+        if (isSelectArr.length == 0) {
+            msg = [
+                `没有找到任何与提供图片相似度高于 ${similarityRate}% 的结果,\n`,
+                '您也可以通过这个链接来自行查找.\n',
+                `${saucenaoUrl + '?url=' + imgUrl}`
+            ]
+            this.e.reply(msg, true, { recallMsg: 90 })
+            return
+        }
+
+        let forwardMsg = []
+        let forwardMsgArr = []
+        for (let obj2 of isSelectArr) {
+            logger.info(obj2[0])
+            forwardMsg = [
+                `[+] 识图结果 ${isSelectArr.indexOf(obj2) + 1}` + '\n'
+            ]
+            forwardMsg.push(`结果相似度: ${obj2[1]}%` + '\n')
+            for (let objName in obj2[0].data) {
+                if (objName == 'ext_urls') {
+                    for (let url of obj2[0].data[objName]) {
+                        forwardMsg.push(`当前识图结果链接 ${obj2[0].data[objName].indexOf(url) + 1}: ${url}` + '\n')
+                    }
+                }
+                else {
+                    if (tags[objName])
+                        forwardMsg.push(`${tags[objName]}: ${obj2[0].data[objName]}` + '\n')
+                    else
+                        forwardMsg.push(`${objName}: ${obj2[0].data[objName]}` + '\n')
+                }
+            }
+            forwardMsg.push(segment.image(`${obj2[0].header.thumbnail}`))
+            forwardMsgArr.push(forwardMsg)
+        }
+
+        await this.e.reply(await this.makeForwardMsg(`识图结果\n源链接: ${saucenaoUrl + '?url=' + imgUrl}`, forwardMsgArr, `已列出所有相似度高于 ${similarityRate}% 的 ${numres} 条有效结果`), true, { recallMsg : 120})
+        await this.e.reply('识图结果已发送完毕, 如果没有消息记录, 则表示识图内容被风控', true)
         return
+    }
+
+    async makeForwardMsg(title, forwardMsgArr, end) {
+        let nickname = Bot.nickname
+
+        if (this.e.isGroup) {
+            let info = await Bot.getGroupMemberInfo(this.e.group_id, Bot.uin)
+            nickname = info.card ?? info.nickname
+        }
+
+        let userInfo = {
+            user_id: Bot.uin,
+            nickname
+        }
+
+        let forwardMsg = [
+            {
+                ...userInfo,
+                message: title
+            }
+        ]
+
+        for (let msg of forwardMsgArr) {
+            forwardMsg.push(
+                {
+                    ...userInfo,
+                    message: msg
+                }
+            )
+        }
+
+        if (end) {
+            forwardMsg.push({
+                ...userInfo,
+                message: end
+            })
+        }
+
+        logger.info(forwardMsg)
+
+        /** 制作转发内容 */
+        if (this.e.isGroup) {
+            forwardMsg = await this.e.group.makeForwardMsg(forwardMsg)
+        } else {
+            forwardMsg = await this.e.friend.makeForwardMsg(forwardMsg)
+        }
+
+        return forwardMsg
     }
 
     // 主要逻辑
@@ -144,7 +254,7 @@ export class saucenao extends plugin {
                 return
             }
 
-            // 2. 回复查询模式
+            // 2. 引用回复查询模式
             if (this.e.source) {
 
                 let targetSource
@@ -164,14 +274,13 @@ export class saucenao extends plugin {
                 }
 
                 if (!this.e.img) {
-                    await this.e.reply('引用目标没有图片可供查询', true, 110)
+                    await this.e.reply('引用目标没有图片可供查询', true)
                     return
                 }
 
                 this.saucenaoSearch(e)
                 return
             }
-
 
             return
 
@@ -195,6 +304,8 @@ export class saucenao extends plugin {
         }
         catch (err) {
             logger.info(err)
+            await this.e.reply('程序出错, 请查看日志')
+            return
         }
     }
 }
