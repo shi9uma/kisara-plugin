a3bb2a5bf42b4a55804919ee1ec76f4d14066abc
wkyuu <tataramoriko@163.com>
2023-02-14 12:59:30 +0800

    process

--- apis.js
+++ apis.js
@@ -1,8 +1,7 @@
-import { segment } from 'oicq'
-
 import lodash from 'lodash'
 import moment from 'moment'
 import fetch from "node-fetch"
+import axios from 'axios'
 
 import plugin from '../../lib/plugins/plugin.js'
 
@@ -71,8 +70,8 @@ export class tiangou extends plugin {
         let apiUrl = 'https://apis.tianapi.com/tiangou/index?key=' + apis.tiangou
         apiUrl = encodeURI(apiUrl)
         let response = await fetch(apiUrl)
-        let {code, msg, result} = await response.json()
-        if(code != '200'){
+        let { code, msg, result } = await response.json()
+        if (code != '200') {
             await this.reply(`[+] 舔狗日志：\n${codes[code]}`, true)
             return
         }
@@ -81,61 +80,121 @@ export class tiangou extends plugin {
     }
 }
 
-// 诗词美句
-export class moodpoetry extends plugin {
+// saucenao 识图
+export class saucenao extends plugin {
     constructor() {
         super({
-            name: '诗词',
-            dsc: '诗词美句',
+            name: '识图',
+            dsc: '利用 saucenao.com 的 api 接口来识图',
             event: 'message',
             priority: 5000,
             rule: [
                 {
-                    reg: "^(来点)?诗词(.*)$",
-                    fnc: 'moodpoetry'
+                    reg: '^#?识图测试$',
+                    fnc: 'saucenaoCheckType'
                 }
             ]
         })
     }
 
-    async moodpoetry(e) {
-        let theme = {
-            1: '离别',
-            2: '人生',
-            3: '生活',
-            4: '四季'
-        }
-        let apiUrl = 'https://apis.tianapi.com/moodpoetry/index?key=' + apis.moodpoetry
-        let searchMsg = e.msg.replace(/(来点诗词 )|(诗词 )/g, '')
-        let flag = e.msg.indexOf('--theme')
-        let type
-        if (flag != -1) {
-            type = e.msg.slice(flag + '--theme'.length + 1)
-            flag = 1
-        }
-        else {
-            type = null
-            flag = 0
-        }
+    async saucenaoSearch(e) {
 
-        if (flag == 1){
-            apiUrl += '&type=' + type
-        }
-
-        apiUrl = encodeURI(apiUrl)
-        let response = await fetch(apiUrl)
-        let {code, msg, result} = await response.json()
-        if(code != '200'){
-            await this.reply(`[+] 诗词美句：\n${codes[code]}`, true)
+        if (apis.saucenao == '') {
+            e.reply('未提供有效的 api_key')
             return
         }
-        let msgReply = [
-            '  [+] 诗词美句：\n',
-            `  主题：${theme[result.type]}\n`,
-            `「${result.title}」 - ${result.author}\n`,
-            `「${result.content}」\n`,
-        ]
-        await this.reply(msgReply, true)
+
+        let imgUrl = e.img[0]
+        let response = await axios.get(
+            'https://saucenao.com/search.php',
+            {
+                params: {
+                    url: imgUrl,
+                    db: 999,
+                    api_key: apis.saucenao,
+                    output_type: 2,
+                    numres: 3
+                }
+            }
+        ).catch((error) => {
+            if (error) {
+                e.reply(`识图 api 无反应, 状态：${error}`, true)
+                logger.info(response)
+                return
+            }
+        })
+        let responseData = response.data.results
+        logger.info(responseData)
         return
+    }
+
+    // 主要逻辑
+    async saucenaoCheckType(e) {
+        try {
+            // 防止滥用
+            let checkPrivate = (this.e.isGroup || this.e.isMaster) ? true : false
+            if (!checkPrivate) {
+                this.e.reply('为防止滥用, 已禁止私聊使用')
+                return
+            }
+
+            // 1. 带图查询模式
+            if (this.e.img) {
+                this.saucenaoSearch(e)
+                return
+            }
+
+            // 2. 回复查询模式
+            if (this.e.source) {
+
+                let targetSource
+                if (this.e.isGroup) {
+                    targetSource = (await this.e.group.getChatHistory(this.e.source.seq, 1)).pop()?.message
+                } else {
+                    targetSource = (await this.e.friend.getChatHistory(this.e.source.time, 1)).pop()?.message
+                }
+
+                if (targetSource) {
+                    for (let obj of targetSource) {
+                        if (obj.type == 'image') {
+                            this.e.img = [obj.url]
+                            break
+                        }
+                    }
+                }
+
+                if (!this.e.img) {
+                    await this.e.reply('引用目标没有图片可供查询', true, 110)
+                    return
+                }
+
+                this.saucenaoSearch(e)
+                return
+            }
+
+
+            return
+
+
+
+            // 3. 延时查询模式(未完成)
+            let time
+            if (this.e.isGroup) {
+                time = 30
+                this.reply(`请在 ${time}s 内发送想要识图的图片`)
+            } else {
+                time = 10
+                this.reply(`请在 ${time}s 内发送想要识图的图片`)
+            }
+
+            /** 主循环计时 */
+            setTimeout(() => {
+                e.reply('操作超时已取消', true)
+            }, time * 1000)
+            return
+        }
+        catch (err) {
+            logger.info(err)
+        }
     }
 }
